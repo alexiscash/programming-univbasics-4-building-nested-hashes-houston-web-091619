87882bc5903e0322d7057e2e2e3b6ca82b081210
Alexis Rangel <alexiscash97@gmail.com>
2019-09-03 02:23:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,14 +25,9 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  hash = monopoly_with_third_tier
-  hash[:railroads][:names].each do |elem|
-    puts hash[:railroads][:names][elem][0] = '5'
-  end
-  hash[:railroads][:names]
+  {railroads: {pieces: 4, rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}, names: {reading_railroad: {mortgage_value: '$5'}, pennsylvania_railroad: {mortgage_value: '$5'}, b_and_o_railroad: {mortgage_value: '$5'}, shortline_railroad: {mortgage_value: '$5'}}}}
 end
 
-puts monopoly_with_fourth_tier
 
 
 
