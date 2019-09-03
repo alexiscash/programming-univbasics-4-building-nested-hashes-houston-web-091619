9140c1b236bddce3daef5e9a3dfaf400c413166c
Alexis Rangel <alexiscash97@gmail.com>
2019-09-03 02:05:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,7 @@ def monopoly_with_fourth_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   hash = monopoly_with_third_tier
-  hash[:railroads][:names]
+  hash[:railroads][:names].each
 end
 
 puts monopoly_with_fourth_tier
